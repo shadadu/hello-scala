adac5eff03ce9b0647e6812d5a28242728553b93
Shad Adu <sxantwi@gmail.com>
2016-06-20 13:07:26 -0400

    update results format

--- build.sbt
+++ build.sbt
@@ -8,4 +8,10 @@ libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "1.6.0"
 
+//resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
+
+resolvers += "Spark Packages Repo" at "https://raw.githubusercontent.com/ankurdave/maven-repo/master"
+
+//libraryDependencies += "amplab" %% "spark-indexedrdd" % "0.3"
+
 fork in run := true
--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -12,7 +12,7 @@ object Hello  {
   def randomEdges(rndm: scala.util.Random, N: Int, nTries:Int): List[Int] = {
     /*
      Generate pair of random numbers from 0 to N-1 to pick pair of vertices to connect with an undirected edge.
-       If the two vertices are the same, pick a new pair of vertices to connect
+     If the two vertices are the same, pick a new pair of vertices to connect
      */
     var v1 = rndm.nextInt(N)
     var v2 = rndm.nextInt(N)
@@ -25,9 +25,7 @@ object Hello  {
     List(v1,v2)
   }
 
-  def max2(a: (Long, Long), b: (Long, Long)): (Long, Long) = {
-    if (a._2 > b._2) a else b
-  }
+  def max2(a: (Long, Long), b: (Long, Long)): (Long, Long) = if (a._2 > b._2) a else b
 
 
   def percolation(N: Long, T: Long, nTries: Int, sc: SparkContext, edgeRate: Int, rndm: Random, randomEdges: (scala.util.Random, Int, Int) => List[Int], writer: PrintWriter): Unit = {
@@ -36,7 +34,7 @@ object Hello  {
     initialize N vertices; each vertex has 2 attributes: Id (Long), name(String)
     */
     val verticesTemp = ListBuffer[(Long, String)]()
-    for (i <- 0 until N.toInt - 1) {
+    for (i <- 0 until N.toInt) {
       verticesTemp += ((i, i.toString + "th"))
     }
     val vRDD = sc.parallelize(verticesTemp.toList) // form RDD of the vertices
@@ -46,13 +44,15 @@ object Hello  {
     GraphX doesn't explicitly allow directed edges, so make the edges undirected by
     forming two reversing edges between the two vertices.
     */
-    var EdgeRDD: RDD[Edge[PartitionID]] = sc.parallelize(Array(Edge(0L, 0L, 1), Edge(0L, 0L, 1)))
+    var EdgeRDD: RDD[Edge[PartitionID]] = sc.parallelize(Array(Edge(0L, 0L, 1), Edge(0L, 0L, 1))).persist
 
     /*
     Run simulation of T events. During each t step, add one (undirected) edge between a randomly-selected
     pair of existing vertices
      */
-    for (t <- 0 until T.toInt - 1) {
+    writer.write("t|maxTris|avgDegree|maxCCsize\n")
+
+    for (t <- 0 until T.toInt ) {
       /*
       To add edgeRate number of new edges during each t, make a bucket to hold new edges that are randomly generated.
       Initialize the bucket with one new randomly edge. bucket size allows to set the rate of increase in size of network
@@ -66,7 +66,7 @@ object Hello  {
       var edgeBucketRDD = nextEdgeRDD
       val edgeBucket = nextEdge
       // Add remaining edgeRate-1 randomly generated edges to the bucket
-      for (n <- 1 to edgeRate - 1) {
+      for (n <- 1 to edgeRate) {
         val currEdges = randomEdges(rndm, N.toInt, nTries)
         v1 = currEdges(0)
         v2 = currEdges(1)
@@ -79,18 +79,24 @@ object Hello  {
       /*
         Make the graph
      */
-      val graph = Graph(vRDD, EdgeRDD, "")
+
+      val graph = Graph(vRDD, EdgeRDD, "").persist()
       /*
       collect data and get metrics
        */
       val cc = graph.connectedComponents().vertices
       val triCounts = graph.triangleCount().vertices
+      val avgDegree = graph.numEdges/N.toDouble
+      graph.unpersist()
       val rankTris: Int = 3
       val maxTris = triCounts.collect.sortWith(_._2 > _._2).take(rankTris)
-      val maxCC = cc.keyBy(_._2).countByKey.reduce(max2)
-      val averageDegree = graph.numEdges/N.toDouble
-      writer.write(t+"|"+maxTris.mkString((""))+"|"+averageDegree+"|"+maxCC.toString+"\n")
+      val maxTrisSum = maxTris.map(c => c._2).sum
+      val maxCC= cc.keyBy(_._2).countByKey.reduce(max2)
+
+      writer.write(t+"|"+ maxTrisSum.toString+"|"+avgDegree+"|"+maxCC._2.toString+"\n")
     }
+
+    EdgeRDD.unpersist()
 
   }
 
@@ -100,12 +106,12 @@ object Hello  {
     val conf = new SparkConf().setAppName("hello-spark").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val T: Long = 20 // the number of events to run; the evolution time of the dynamics
+    val T: Long = 100 // the number of events to run; the evolution time of the dynamics
     val N: Long = 100 // the number of vertices
     val edgeRate: Int = 5 // the number of edges to add during each time step
     val nTries: Int = 5   // the number of times to avoid self-links; to avoid infinite while loop
     val rndm = scala.util.Random
-    val writer = new PrintWriter("output\\log.txt")
+    val writer = new PrintWriter("output\\outputData.csv")
 
     percolation(N,T,nTries,sc,edgeRate,rndm, randomEdges, writer)
 
